ab2c43af487d62ecdf2fc9bff8cc016fe9c36b9d
ccolorado <ccolorado@localhost.localdomain>
2015-12-06 22:12:24 -0600

    Cleanup code

--- branch_drawer.py
+++ branch_drawer.py
@@ -7,28 +7,24 @@ import os
 import os.path
 import subprocess
 
-# Check is there is an in repo config
-# store a unique place per clone to store the drawer directory
 # git config hook.yourhook.yourconfigval value
 
 def get_repo_root_directory():
     repo_root_directory = subprocess.check_output( "git rev-parse --show-toplevel", shell = True )
-    return repo_root_directory
+    return repo_root_directory.strip()
 
 def get_current_branch_name():
-    # Remove extra line breaks and whitespaces
     current_branch_name = subprocess.check_output( "basename $(git symbolic-ref -q HEAD)", shell = True )
-    # current_branch_name = subprocess.check_output( ["basename", current_branch_name] )
-    return current_branch_name
+    return current_branch_name.strip()
 
 def get_repo_name():
-    # Figure out getting git reponame ( regarthless of the name of the repo root directory )
-    repo_name = subprocess.check_output( 'git something', shell = True )
-    return " idklol "
+    repo_name = subprocess.check_output( "basename %s" % get_repo_root_directory(), shell = True )
+    return repo_name.strip()
 
 def parse_args():
-    print(get_current_branch_name())
-    print("x")
+    print("Repo Name : " + get_repo_name())
+    print("Repo root directory : " + get_repo_root_directory())
+    print("Current Branch : " + get_current_branch_name())
 
 def main():
     global CONF
